fcffe41133a89190c333fc3af063086f67581589
parikesitad-pm <parikesit.ad@gmail.com>
2021-05-12 00:43:46 +0700

    innitial setup

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').parse()
+  require('dotenv').config()
 }
 
 const express = require('express')
@@ -16,12 +16,12 @@ app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Monggose'))
-
 
 app.use('/', indexRouter)
 
